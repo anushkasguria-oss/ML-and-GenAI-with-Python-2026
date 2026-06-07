a9ca9a7603e7f3281969a91ead1787e7f35054d8
Anushka Stacy Guria <anushkasguria@gmail.com>
2026-06-07 13:40:51 +0530

    Add files via upload

---
+++ Assignment_1/Anushka_Stacy_Guria_03701172025/1.py
@@ -0,0 +1,52 @@
+#area of rectangle
+l=20
+b=4
+area=l*b
+print("Area=",area)
+
+#simple intrest
+p=1000
+r=5
+t=2
+SI=(p*r*t)/100
+print("Simple Intrest=",SI)
+
+#Celsius to Fahrenheit
+c=32
+f=(c*9/5)+32
+print("Celsius to Fahrenheit=",f)
+
+#Average of three numbers
+a=60
+b=45
+c=39
+avg=(a+b+c)/3
+print("Average=",avg)
+
+#Square and cube of a number
+x=7
+sq=x*x
+cu=x*x*x
+print("Square=",sq)
+print("Cube=",cu)
+
+#Swap two numbers without third variable
+x=10
+y=1
+print("x=",y)
+print("y=",x)
+
+#Student report program
+
+#name of student
+name=input("enter name of student: ")
+#marks of each subject
+maths=int(input("enter marks: "))
+english=int(input("enter marks: "))
+evs=int(input("enter marks: "))
+#total score of all subjects
+t= maths+english+evs
+print("Total score=",t)
+#prectage acquired
+p=((maths+english+evs)/300)*100
+print("Percentage:",p)
